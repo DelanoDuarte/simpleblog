b479b39146174e6976fd08af80128ea193bf4f64
DelanoJunior <delano.duarte@gmail.com>
2017-02-04 21:55:25 -0300

    Primeiro Commit aplicação.Configuração do ambiente realizada !

---
+++ src/main/java/br/com/simpleblog/restconfig/RestApplicationConfig.java
@@ -0,0 +1,9 @@
+package br.com.simpleblog.restconfig;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest/api")
+public class RestApplicationConfig extends Application {
+
+}
